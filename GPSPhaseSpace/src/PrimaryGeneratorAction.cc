5d118f4344d3825897150e0627e607d1bf5f2b13
Amy <amy.gonzalez.vlz@gmail.com>
2026-01-21 08:18:20 -0700

    21 de Enero del 2026 - 2

--- GPSPhaseSpace/src/PrimaryGeneratorAction.cc
+++ GPSPhaseSpace/src/PrimaryGeneratorAction.cc
@@ -7,14 +7,34 @@
 #include "G4SystemOfUnits.hh"
 #include "Randomize.hh"
 #include "G4ThreeVector.hh"
+#include "TFile.h"
+#include "TROOT.h"
+#include <iostream>
+#include <cmath> 
 // #include "CLHEP/Units/SystemOfUnits.hh"
 
 PrimaryGeneratorAction::PrimaryGeneratorAction(){
-  fGun = new G4ParticleGun(1);
-  G4ParticleDefinition * electron = G4ParticleTable::GetParticleTable()->FindParticle("e-");
-  fGun->SetParticleDefinition(electron);
-  fGun->SetParticleEnergy(6. * eV);
-  fGun->SetParticlePosition(G4ThreeVector(0));
+  // fGun = new G4ParticleGun(1);
+  // G4ParticleDefinition * electron = G4ParticleTable::GetParticleTable()->FindParticle("e-");
+  // fGun->SetParticleDefinition(electron);
+  // fGun->SetParticleEnergy(6. * eV);
+  // fGun->SetParticlePosition(G4ThreeVector(0));
+  file=TFile::Open("PhaseSpace.root");
+  tree= (TTree*)file->Get("PhSp");
+  tree->SetBranchesAddress("x", &x);
+  tree->SetBranchesAddress("y", &y);
+  tree->SetBranchesAddress("z", &z);
+  tree->SetBranchesAddress("px", &px);
+  tree->SetBranchesAddress("py", &py);
+  tree->SetBranchesAddress("pz", &px);
+  tree->SetBranchesAddress("E", &E);
+  tree->SetBranchesAddress("t", &t);
+  tree->SetBranchesAddress("w", &w);
+  tree->SetBranchesAddress("pdg", &pdg);
+
+  nEntries = tree->GetEntries();
+  currentEntry = 0;
+  
 }
 
 
@@ -34,7 +54,25 @@ void PrimaryGeneratorAction::GeneratePrimaries(G4Event* event) {
   // G4cout << "Generating primary particle" << G4endl;
 
 
-  file=TFile::Open("PhaseSpace.root");
-  tree= (TTree*)file->Get("PhSp");
-  
+  if(currendEntry >= nEntries) {
+    G4Exeption("PrimaryGeneratorAction",
+	       "EndOfPhaseSpace",
+	       FatalExeption,
+	       "Phase space file exhausted");
+  }
+
+  tree->GetEntry(currentEntry++);
+  auto ParticleDef = G4ParticleTable::GetParticleTable()->FindParticle(pdg);
+  G4ThreeVector Pos = G4ThreeVector(x * mm,y * mm,z * mm);
+  G4ThreeVector P = G4ThreeVector(px,py,pz);
+
+  fGun->SetParticleDefinition(ParticleDef);
+  fGun->SetParticlePosition(Pos);
+  fGun->SetParticleMomentumDirection(P.unit);
+  fGun->SetParticleEnergy(E*MeV);
+
+  fGun->GetPrimaryVertex(event);
+  event->GetPrimaryVertex()->SetWeight(w);
+
+  G4cout << "Generating primary particle" << G4endl;
 }
